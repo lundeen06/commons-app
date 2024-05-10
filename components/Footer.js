8f5ff0ca91845f120efa276f7824daa19aad3340
Lundeen Cahilly <lundeen.cahilly@gmail.com>
2024-05-10 08:29:45 -0400

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -24,20 +24,20 @@ export const Footer = () => {
   return (
     <View style={styles.footer}>
       <TouchableOpacity onPress={() => console.log('Home pressed')}>
-        <View> <Icon name="home" size={30} style={styles.icon} /> </View>
+        <Text> <Icon name="home" size={30} style={styles.icon} /> </Text>
       </TouchableOpacity>
-      {/* <TouchableOpacity onPress={() => console.log('Map pressed')}> */}
-        {/* <Icon name="map" size={30} style={styles.icon} /> */}
-      {/* </TouchableOpacity> */}
-      {/* <TouchableOpacity onPress={() => console.log('Book/Menu pressed')}> */}
-        {/* <Icon name="book" size={30} style={styles.icon} /> */}
-      {/* </TouchableOpacity> */}
-      {/* <TouchableOpacity onPress={() => console.log('Teams pressed')}> */}
-        {/* <Icon name="people" size={30} style={styles.icon} /> Adjusted to 'people' for teams */}
-      {/* </TouchableOpacity> */}
-      {/* <TouchableOpacity onPress={() => console.log('Profile pressed')}> */}
-        {/* <Icon name="person" size={30} style={styles.icon} /> */}
-      {/* </TouchableOpacity> */}
+      <TouchableOpacity onPress={() => console.log('Map pressed')}>
+        <Text> <Icon name="map" size={30} style={styles.icon} /> </Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => console.log('Book/Menu pressed')}>
+        <Text> <Icon name="book" size={30} style={styles.icon} /> </Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => console.log('Teams pressed')}>
+        <Text> <Icon name="people" size={30} style={styles.icon} /> </Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => console.log('Profile pressed')}>
+        <Text> <Icon name="person" size={30} style={styles.icon} /> </Text>
+      </TouchableOpacity>
     </View>
   );
 };
