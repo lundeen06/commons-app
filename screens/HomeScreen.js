697f5cd9d8418e25dd1100744ab19952b6b7add1
Lundeen Cahilly <lundeen.cahilly@gmail.com>
2024-05-16 11:36:29 -0400

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,8 +2,14 @@ import React from "react";
 import { View, StyleSheet, Button, Text } from "react-native";
 import { signOut } from "firebase/auth";
 import { Footer } from "../components/Footer";
-
 import { auth } from "../config";
+
+testUser = {
+  firstName: "first name",
+}
+
+user = testUser
+
 
 export const HomeScreen = () => {
   const handleLogout = () => {
@@ -14,7 +20,10 @@ export const HomeScreen = () => {
     <View style={styles.main}>
       {/* <Button title="Sign Out" onPress={handleLogout} /> */}
       {/* <Text> home </Text> */}
-      <Text> test </Text>
+      <View style={styles.greeting}>
+        <Text style={styles.h1}>Hello, {user.firstName}! </Text>
+        <Text style={styles.subtitle}>See your dining options today!</Text>
+      </View>
       <Footer />
     </View>
   );
@@ -28,5 +37,20 @@ const styles = StyleSheet.create({
     alignItems: 'center', // Centers content horizontally if needed
     width: '100%', // Ensures the view takes full width
     height: '100%', // Ensures the view takes full height
+    backgroundColor: '#fff', 
   },
+  greeting: {
+    width: '100%',
+    marginLeft: 100,
+    marginTop: 20,
+  },
+  h1: {
+    fontSize: 30,
+    fontWeight: '600',
+  },
+  subtitle: {
+    color: '#888',
+    fontSize: 12,
+    fontWeight: '600',
+  }
 });
