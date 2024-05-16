b8bd1bd0250192d8bde9ff25ff918b969ce561d4
Lundeen Cahilly <lundeen.cahilly@gmail.com>
2024-05-16 11:36:33 -0400

    Update AppStack.js

--- navigation/AppStack.js
+++ navigation/AppStack.js
@@ -8,7 +8,7 @@ const Stack = createStackNavigator();
 export const AppStack = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="[TITLE]" component={HomeScreen} />
+      <Stack.Screen name="dev-home" component={HomeScreen} />
     </Stack.Navigator>
   );
 };
