369ecc8f074456843e4c5cd7748cbb3adfbc4fbb
Lundeen Cahilly <lundeen.cahilly@gmail.com>
2024-05-17 10:43:58 -0400

    Create DiningCapacity.js

---
+++ components/DiningCapacity.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, TouchableOpacity, StyleSheet, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { Images, Colors, auth } from "../config";
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: Colors.lightGray, // Ensure this color is defined in your Colors object
+    width: 150,
+    height: 150,
+    borderRadius: 20,
+  },
+});
+
+export const DiningCapacity = () => {
+  return (
+    
+    <TouchableOpacity onPress={() => console.log('capacity info pressed')}>
+        <View style={styles.container}>
+
+        </View>
+    </TouchableOpacity>
+   
+  );
+};
