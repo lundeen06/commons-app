9aaef75fc92a5319f4d8fb7e5d94470dc7d1bf04
Lundeen Cahilly <lundeen.cahilly@gmail.com>
2024-05-17 10:54:09 -0400

    Create WalkingDistance.js

---
+++ components/WalkingDistance.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { View, TouchableOpacity, StyleSheet, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { Images, Colors, auth } from "../config";
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: Colors.lightGray, // Ensure this color is defined in your Colors object
+    width: 165,
+    height: 75,
+    borderRadius: 30,
+  },
+});
+
+export const WalkingDistance = () => {
+  return (
+    <TouchableOpacity onPress={() => console.log('capacity info pressed')}>
+        <View style={styles.container}>
+
+        </View>
+    </TouchableOpacity>
+   
+  );
+};
