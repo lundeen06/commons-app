da792e7d7cd2204241c8459c0b57cce161dbb538
Lundeen Cahilly <lundeen.cahilly@gmail.com>
2024-05-19 17:10:06 -0400

    Create CommunityScreen.js

---
+++ screens/CommunityScreen.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { View, StyleSheet, Button, Text, TouchableOpacity, Image } from "react-native";
+import { signOut } from "firebase/auth";
+import { Footer } from "../components/Footer";
+import { Images, Colors, auth } from "../config";
+
+export const CommunityScreen = ({ navigation }) => {
+  const handleLogout = () => {
+    signOut(auth).catch((error) => console.log("Error logging out: ", error));
+  };
+
+  return (
+    <View style={styles.main}>
+      <Text> [community screen goes here] </Text>
+      <Footer navigation={navigation} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#fff',
+  },
+  footer: {
+    justifySelf: 'flex-start',
+  },
+});
