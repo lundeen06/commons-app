9ac724450217fe5012ca7493b5af323d837d39ee
Lundeen Cahilly <lundeen.cahilly@gmail.com>
2024-05-19 00:34:12 -0400

    Update DiningCapacity.js

--- components/DiningCapacity.js
+++ components/DiningCapacity.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { View, TouchableOpacity, StyleSheet, Text } from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
+import { View, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
+import Svg, { Circle, Rect } from 'react-native-svg';
+
 import { Images, Colors, auth } from "../config";
+
+const capacity = 0.25;
 
 const styles = StyleSheet.create({
   container: {
@@ -9,7 +12,7 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: Colors.darkGreen, // Ensure this color is defined in your Colors object
+    backgroundColor: Colors.darkGreen,
     width: 165,
     height: 165,
     borderRadius: 25,
@@ -17,13 +20,26 @@ const styles = StyleSheet.create({
 });
 
 export const DiningCapacity = () => {
-  return (
-    
-    <TouchableOpacity onPress={() => console.log('capacity info pressed')}>
-        <View style={styles.container}>
+  const radius = 70;
+  const circumference = radius * 2 * Math.PI;
+  const strokeDashoffset = circumference - (capacity * circumference);
 
-        </View>
+  return (
+    <TouchableOpacity onPress={() => console.log('capacity info pressed')}>
+      <View style={styles.container}>
+        <Svg height="100%" width="100%">
+          <Rect x="0" y="0" rx="25" ry="25" width="100%" height="100%" fill={Colors.darkGreen} />
+          <Circle
+            stroke={Colors.white}
+            strokeWidth="10"
+            fill="transparent"
+            strokeDasharray={`${circumference} ${circumference}`}
+            strokeDashoffset={strokeDashoffset}
+            cx="80"
+            cy="80"
+          />
+        </Svg>
+      </View>
     </TouchableOpacity>
-   
   );
 };
