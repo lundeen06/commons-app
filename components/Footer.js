75e07bd401b6f73f0edbe68d730695b3d36e6148
Lundeen Cahilly <lundeen.cahilly@gmail.com>
2024-05-18 20:22:41 -0400

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -8,15 +8,18 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-around',
     alignItems: 'center',
-    backgroundColor: Colors.red, // Ensure this color is defined in your Colors object
+    backgroundColor: Colors.white, // Ensure this color is defined in your Colors object
     padding: 10,
+    borderTopColor: Colors.lightGray, 
+    borderTopWidth: 1,
+    borderTopStyle: 'solid',
     paddingBottom: 20,
     width: '100%',
     height: 100,
   },
   icon: {
     marginLeft: 20, // Increased margin for better spacing
-    color: Colors.white,
+    color: Colors.darkGreen,
     fontSize: 25, // Increased font size for better visibility
     marginVertical: 10, // Adjusted vertical margin for better spacing
     fontWeight: '600',
@@ -28,19 +31,19 @@ export const Footer = () => {
   return (
     <View style={styles.footer}>
       <TouchableOpacity onPress={() => console.log('Home pressed')}>
-        <Text> <Icon name="home" style={styles.icon} /> </Text>
+        <Text> <Icon name="home-outline" style={styles.icon} /> </Text>
       </TouchableOpacity>
       <TouchableOpacity onPress={() => console.log('Map pressed')}>
-        <Text> <Icon name="map" style={styles.icon} /> </Text>
+        <Text> <Icon name="map-outline" style={styles.icon} /> </Text>
       </TouchableOpacity>
       <TouchableOpacity onPress={() => console.log('Book/Menu pressed')}>
-        <Text> <Icon name="book" style={styles.icon} /> </Text>
+        <Text> <Icon name="book-outline" style={styles.icon} /> </Text>
       </TouchableOpacity>
       <TouchableOpacity onPress={() => console.log('Teams pressed')}>
-        <Text> <Icon name="people" style={styles.icon} /> </Text>
+        <Text> <Icon name="people-outline" style={styles.icon} /> </Text>
       </TouchableOpacity>
       <TouchableOpacity onPress={() => console.log('Profile pressed')}>
-        <Text> <Icon name="person" style={styles.icon} /> </Text>
+        <Text> <Icon name="person-outline" style={styles.icon} /> </Text>
       </TouchableOpacity>
     </View>
   );
