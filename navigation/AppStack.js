09979091d4e1e4a2123538115cdd56bc0d0ed6a1
Lundeen Cahilly <lundeen.cahilly@gmail.com>
2024-05-19 14:56:53 -0400

    Update AppStack.js

--- navigation/AppStack.js
+++ navigation/AppStack.js
@@ -1,14 +1,15 @@
 import * as React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 
-import { HomeScreen } from "../screens";
+import { HomeScreen, MapScreen } from "../screens";
 
 const Stack = createStackNavigator();
 
 export const AppStack = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="dev-home" component={HomeScreen} />
+      {/* <Stack.Screen name="dev-home" component={HomeScreen} /> */}
+      <Stack.Screen name="dev-home" component={MapScreen} />
     </Stack.Navigator>
   );
 };
