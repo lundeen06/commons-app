96091a2d95756abdfd8f8d7581e9bfd2047357bf
Lundeen Cahilly <lundeen.cahilly@gmail.com>
2024-05-19 22:05:05 -0400

    Add selectors

--- screens/MenuScreen.js
+++ screens/MenuScreen.js
@@ -1,21 +1,48 @@
-import React from "react";
-import { View, StyleSheet, Button, Text, TouchableOpacity, Image } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet, TouchableOpacity, Text, Image } from "react-native";
 import { signOut } from "firebase/auth";
 import { Footer } from "../components/Footer";
 import { Images, Colors, auth } from "../config";
 
 export const MenuScreen = ({ navigation }) => {
+  const [selectedOptions, setSelectedOptions] = useState({});
+
   const handleLogout = () => {
     signOut(auth).catch((error) => console.log("Error logging out: ", error));
+  };
+
+  const options = ['Open now', 'Within.5mi', 'Not busy'];
+
+  const toggleSelector = (option) => {
+    setSelectedOptions(prevState => ({
+     ...prevState,
+      [option]:!prevState[option]
+    }));
   };
 
   return (
     <View style={styles.main}>
       <View style={styles.hero}> 
         <Text style={styles.h1}>Menus and Hours</Text>
+        <View style={styles.selectorContainer}> 
+          {options.map((option, index) => (
+            <TouchableOpacity 
+              key={index}
+              style={[
+                styles.selector, 
+                selectedOptions[option]? {backgroundColor: Colors.font} : {backgroundColor: Colors.white}
+              ]}
+              onPress={() => toggleSelector(option)}
+            >
+              <Text style={[
+                styles.selectorText, 
+                selectedOptions[option]? {color: Colors.white} : {color: Colors.font}
+              ]}>{option}</Text>
+            </TouchableOpacity>
+          ))}
+        </View>
       </View>
 
-      
       <Footer navigation={navigation} />
     </View>
   );
@@ -36,9 +63,10 @@ const styles = StyleSheet.create({
     justifySelf: 'flex-start',
   },
   hero: {
-    width: '100%',
+    width: 'calc(100% - 100px)',
+    height: 50,
     marginLeft: 50,
-    // marginTop: 20,
+    marginRight: 50,
   },
   h1: {
     fontSize: 32,
@@ -55,4 +83,29 @@ const styles = StyleSheet.create({
     fontSize: 12,
     fontWeight: '600',
   },
+  selectorContainer: {
+    flex: 2, 
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignContent: 'center',
+    width: '100%',
+    marginTop: 10,
+  },
+  selector: {
+    width: 90,
+    height: 30,
+    borderRadius: 30,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    justifyContent: 'center',
+    // padding: 5,
+    backgroundColor: Colors.white, // Ensure this color is defined in your Colors object
+    borderColor: Colors.lightGray,
+    borderStyle: 'solid',
+    borderWidth: 1,
+  },
+  selectorText: {
+    color: Colors.font,
+  }
 });
